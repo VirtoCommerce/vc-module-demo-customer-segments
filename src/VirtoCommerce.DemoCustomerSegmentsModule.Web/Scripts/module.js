c19e9a9574cf535490e1b3d63b1f39e0985d8f54
trueboroda <superiorboroda@gmail.com>
2020-12-02 09:31:15 +0300

    Move All Codes related with CustomerSegments from DemoSolutionFeaturesModules
    Also module name in scipts was corrected.
    And localization terms was corrected.

--- src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/module.js
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/module.js
@@ -1,42 +1,20 @@
 // Call this to register your module to main application
-var moduleName = "virtoCommerce.demoCustomerSegmentsModule";
+var moduleName = "virtoCommerce.DemoCustomerSegmentsModule";
 
 if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
 }
 
 angular.module(moduleName, [])
-    .config(['$stateProvider', '$urlRouterProvider',
-        function ($stateProvider, $urlRouterProvider) {
-            $stateProvider
-                .state('workspace.virtoCommerceDemoCustomerSegmentsModuleState', {
-                    url: '/virtoCommerce.demoCustomerSegmentsModule',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        '$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'virtoCommerce.demoCustomerSegmentsModule.helloWorldController',
-                                template: 'Modules/$(virtoCommerce.demoCustomerSegmentsModule)/Scripts/blades/hello-world.html',
-                                isClosingDisabled: true
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
-        }
-    ])
-    .run(['platformWebApp.mainMenuService', 'platformWebApp.widgetService', '$state',
-        function (mainMenuService, widgetService, $state) {
-            //Register module in main menu
-            var menuItem = {
-                path: 'browse/virtoCommerce.demoCustomerSegmentsModule',
-                icon: 'fa fa-cube',
-                title: 'VirtoCommerce.DemoCustomerSegmentsModule',
-                priority: 100,
-                action: function () { $state.go('workspace.virtoCommerceDemoCustomerSegmentsModuleState'); },
-                permission: 'virtoCommerceDemoCustomerSegmentsModule:access'
-            };
-            mainMenuService.addMenuItem(menuItem);
+    .run(['virtoCommerce.marketingModule.marketingMenuItemService',
+        function (marketingMenuItemService) {
+            marketingMenuItemService.register({
+                id: 'customerSegments',
+                name: 'Customer segments',
+                entityName: 'customerSegment',
+                icon: 'fa fa-pie-chart',
+                controller: 'virtoCommerce.DemoCustomerSegmentsModule.customerSegmentDetailController',
+                template: 'Modules/$(virtoCommerce.DemoCustomerSegmentsModule)/Scripts/blades/customerSegment-detail.tpl.html'
+            });
         }
     ]);
--- src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/resources/virto-commerce.demo-customer-segments-module-api.js
+++
@@ -1,4 +0,0 @@
-angular.module('virtoCommerce.demoCustomerSegmentsModule')
-    .factory('virtoCommerce.demoCustomerSegmentsModule.webApi', ['$resource', function ($resource) {
-        return $resource('api/VirtoCommerceDemoCustomerSegmentsModule');
-}]);
