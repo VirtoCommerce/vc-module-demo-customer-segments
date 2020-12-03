10a92fdb731afb796553b739001f15df81bf54e2
Ilia <ilya.ivanov6@gmail.com>
2020-12-03 16:13:11 +0200

    Fix localization

--- src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/blades/customerSegment-rule.js
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/blades/customerSegment-rule.js
@@ -58,7 +58,7 @@ angular.module('virtoCommerce.DemoCustomerSegmentsModule')
         $scope.editProperties = function () {
             var newBlade = {
                 id: 'propertiesEditor',
-                title: 'demoSolutionFeaturesModule.blades.customer-segment-property-values.title',
+                title: 'demoCustomerSegmentsModule.blades.customer-segment-property-values.title',
                 headIcon: 'fa-pie-chart',
                 controller: 'virtoCommerce.DemoCustomerSegmentsModule.customerSegmentPropertyValuesController',
                 template: 'Modules/$(virtoCommerce.DemoCustomerSegmentsModule)/Scripts/blades/customerSegment-property-values.tpl.html',
