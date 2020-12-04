1ce4e1654f54323792bcf65579fffaa06d059667
Ilia <ilya.ivanov6@gmail.com>
2020-12-04 16:52:12 +0200

    Edit created customer segment

--- src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/blades/customerSegment-detail.js
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Web/Scripts/blades/customerSegment-detail.js
@@ -1,12 +1,26 @@
 angular.module('virtoCommerce.DemoCustomerSegmentsModule')
 .controller('virtoCommerce.DemoCustomerSegmentsModule.customerSegmentDetailController',
-    ['$scope', 'platformWebApp.bladeNavigationService', function ($scope, bladeNavigationService) {
+    ['$scope', 'platformWebApp.bladeNavigationService', 'virtoCommerce.DemoCustomerSegmentsModule.customerSegmentsApi', function ($scope, bladeNavigationService, customerSegmentsApi) {
         const blade = $scope.blade;
         blade.headIcon = 'fa-pie-chart';
-        blade.isLoading = false;
         blade.activeBladeId = null;
 
         blade.currentEntity = {};
+
+        blade.refresh = function () {
+            if (blade.isNew) {
+                customerSegmentsApi.new({},
+                    data => {
+                        blade.currentEntity = data;
+                        blade.isLoading = false;
+                    });
+            } else {
+                customerSegmentsApi.get({ id: blade.currentEntityId }, data => {
+                    blade.currentEntity = data;
+                    blade.isLoading = false;
+                });
+            }
+        }
 
         $scope.canSave = () => {
             return false;
@@ -52,8 +66,9 @@ angular.module('virtoCommerce.DemoCustomerSegmentsModule')
                 $scope.filledPropertiesCount += blade.currentEntity.isActive !== undefined ? 1 : 0;
                 $scope.filledPropertiesCount += blade.currentEntity.startDate ? 1 : 0;
                 $scope.filledPropertiesCount += blade.currentEntity.endDate ? 1 : 0;
-                $scope.filledPropertiesCount += blade.currentEntity.storeIds ? 1 : 0;
+                $scope.filledPropertiesCount += blade.currentEntity.storeIds && blade.currentEntity.storeIds.length ? 1 : 0;
             }
         }, true);
 
+        blade.refresh();
     }]);
