305a21a3c464d11646269e9a87eee553979f63cd
Aleksandr Vishniakov <av@virtoway.com>
2020-12-04 10:15:01 +0200

    VDS-334: Fix properties in condition

--- src/VirtoCommerce.DemoCustomerSegmentsModule.Core/Models/DemoConditionPropertyValues.cs
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Core/Models/DemoConditionPropertyValues.cs
@@ -1,15 +1,11 @@
 using System.Collections.Generic;
 using VirtoCommerce.CoreModule.Core.Conditions;
+using VirtoCommerce.Platform.Core.DynamicProperties;
 
 namespace VirtoCommerce.DemoCustomerSegmentsModule.Core.Models
 {
     public class DemoConditionPropertyValues : ConditionTree
     {
-        public IDictionary<string, string[]> Properties { get; set; }
-
-        public IDictionary<string, string[]> GetPropertyValues()
-        {
-            return new Dictionary<string, string[]>();
-        }
+        public ICollection<DynamicObjectProperty> Properties { get; set; } = new List<DynamicObjectProperty>();
     }
 }
