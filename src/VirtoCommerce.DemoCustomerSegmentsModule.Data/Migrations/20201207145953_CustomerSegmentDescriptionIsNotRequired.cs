1e7cc16e6c713454b1b29fd3a6982d0fa7328ad4
trueboroda <superiorboroda@gmail.com>
2020-12-07 18:23:07 +0300

    Description field of segment is not required.

---
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Data/Migrations/20201207145953_CustomerSegmentDescriptionIsNotRequired.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VirtoCommerce.DemoCustomerSegmentsModule.Data.Migrations
+{
+    public partial class CustomerSegmentDescriptionIsNotRequired : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "DemoCustomerSegments",
+                maxLength: 1024,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1024)",
+                oldMaxLength: 1024);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "DemoCustomerSegments",
+                type: "nvarchar(1024)",
+                maxLength: 1024,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 1024,
+                oldNullable: true);
+        }
+    }
+}
