7c0a6254fe88a5ab1ab61a37f6dafcd1cd6d5a01
Dmitry Pushnitsa <d.pushnitsa@gmail.com>
2021-04-20 16:50:31 +0200

    Remove suffix from webpack config

--- src/VirtoCommerce.DemoCustomerSegmentsModule.Web/webpack.config.js
+++ src/VirtoCommerce.DemoCustomerSegmentsModule.Web/webpack.config.js
@@ -32,7 +32,7 @@ module.exports = [
         devtool: false,
         plugins: [
             new webpack.SourceMapDevToolPlugin({
-                namespace: 'VirtoCommerce.DemoCustomerSegmentsModule'
+                namespace: 'VirtoCommerce.DemoCustomerSegments'
             }),
             new CleanWebpackPlugin(rootPath, { verbose: true }),
             new MiniCssExtractPlugin({
